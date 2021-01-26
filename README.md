851b284a4f84d5c19eeab2fe58dffabe98591836
zanya945 <77784793+zanya945@users.noreply.github.com>
2021-01-26 21:43:22 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# main
+# js
 Created with CodeSandbox
---
+++ src/main.js
@@ -0,0 +1,9 @@
+function print() {
+  document.getElementById("demo").innerHTML = "Hello JavaScript";
+  document.getElementById("demo").innerHTML = "w 在搞";
+  document.getElementById("demo").innerHTML = "J 也是";
+  document.getElementById("demo").style.fontsize = "35px";
+  document.getElementById("demo").style.fontsize = "35px";
+  document.getElementById("demo").style.display = "none";
+  document.getElementById("demo").style.display = "block";
+}
